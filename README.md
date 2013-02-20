fcc8c086c093c0c9e890a08b713eb1e56201df35
Keith <keith.strand@gmail.com>
2013-02-20 09:51:13 -0800

    second commit

---
+++ README.md
@@ -0,0 +1,13 @@
+se_app_spec
+app_template_with_rspec_gem
+!! do not push changes to this template !!
+Note:
+  1.  Create a new repo at github.
+  2.  Clone this repo to your local machine.
+  3.  git remote rename origin upstream
+  4.  git remote add origin URL_TO_GITHUB_REPO
+  5.  git push origin master
+Now you can work with it just like any other github repo. To pull in
+patches from upstream, simply run git pull upstream master && git push
+origin master.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseRspecApp::Application.config.secret_token = '31f837f2270d2640cd3f8f47272471bd9f06381956cb1def2860f0c26f71ae082ce86eb6cba90ae3b405251b351c66dfdf7cb85284e7e81698806d7a73559df4'
